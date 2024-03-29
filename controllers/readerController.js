ac82295ee55db24fb54dc4255366d1f4268d41be
Jake Browning <jacobbrowning90@gmail.com>
2024-03-29 18:52:34 +0000

    Update passport.authenticate call

--- controllers/readerController.js
+++ controllers/readerController.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const Reader = require("../models/reader");
 
 // const { body, validationResult } = require("express-validator");
@@ -5,16 +6,40 @@ const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
-const initializePassport = require('../passport-config');
-initializePassport(
-    passport, 
-    // email => Reader.findOne({email: req.body.email}),
-    email => users.find(user => user.email === req.body.email),
-    id => users.find(user => user.id === id),
-);
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      console.log("Authenticating...")
+      try {
+        const user = await Reader.findOne({ email: username });
+        if (!user) {
+          console.log("Incorrect user")
+           return done(null, false, { message: "Incorrect username" });
+        };
+        if (user.password !== password) {
+          console.log("Incorrect password")
+          return done(null, false, { message: "Incorrect password" });
+        };
+        return done(null, reader);
+      } catch(err) {
+        return done(err);
+      };
+    })
+  );
 
-require('dotenv').config();
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await Reader.findById(id);
+      done(null, user);
+    } catch(err) {
+      done(err);
+    };
+  });
 // exports.function_name = asyncHandler(async (req, res, next) => {
 
 // });
@@ -41,16 +66,15 @@ exports.reader_create = asyncHandler(async (req, res, next) => {
     }
 });
 
-exports.reader_login = asyncHandler(async (req, res, next) => {
-    const users = [req.body]// console.log(req.body);
+exports.reader_login = [
+    // res.json(req.body);
     passport.authenticate("local", {
-        successRedirect: "/",
-        failureRedirect: "/login",
-
-      })
+      successRedirect: "/",
+      failureRedirect: "/"
+    })
     // await reader.save();
     // res.json(reader);
-});
+  ]
 
 exports.reader_read = asyncHandler(async (req, res, next) => {
     const reader = await Reader.findById(req.params.id).exec();
